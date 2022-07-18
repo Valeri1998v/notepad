02d6ad662ffc7cde2bf7d51a2a967a347fb7be8b
Валерия <valeriaverbov7@gmail.com>
2022-07-18 12:18:19 +0300

    initial commit

--- tasks.rb
+++ tasks.rb
@@ -1,3 +1,4 @@
+require "date"
 class Tasks < Post
 
   def initialize
@@ -7,8 +8,18 @@ class Tasks < Post
   end
 
   def read_from_console
+    puts "Что надо сделать"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу нужно сделать, укажите дату в формате ДД.ММ.ГГГГ"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strf.time("%Y.%m.%d, %H.%M.%S")} \n\r n\r"
+    deadline = "Крайний срок #{@due_date}"
+    return [deadline, @text, time_string]
   end
 end
